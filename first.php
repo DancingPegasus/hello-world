764d75de7473684ffa7519946a71bb431316168a
DancingPegasus <DancingPegasus@users.noreply.github.com>
2014-11-27 14:23:58 -0800

    Update first.php

--- first.php
+++ first.php
@@ -4,19 +4,13 @@ require('somefile.php');
 
 define ('PAGE_NUM', $_SESSION['page_num']);
 
-switch (PAGE_NUM)
+if (PAGE_NUM < 1)
 {
-   case 0:
-   //error
-   break;
-   
-   case 1: 
-   //beginning of book
-   break;
-   
-   case 2:
-   //book has started
-   break;
+   //invalid page number
+}
+else
+{
+   //display page
 }
 
 ?>
